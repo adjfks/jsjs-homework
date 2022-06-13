7a658d640cf4056446fca2bf1f46c07a15aa87fd
corner <sptdfdzl@163.com>
2022-06-13 16:39:00 +0800

    feat: homework1

--- homework/1/rename.js
+++ homework/1/rename.js
@@ -7,7 +7,17 @@ function transform(root, originName, targetName) {
   return traverse((node, ctx, next) => {
 
     // TODO: 作业代码写在这里
-    if (node.type === 'xxx') {
+    if (node.type === 'VariableDeclarator' || node.type === 'FunctionDeclaration' && node.id) {
+      node.id.name = node.id.name === originName ? targetName : node.id.name
+    }
+
+    if (node.type === 'MemberExpression' && node.object) {
+      node.object.name = node.object.name === originName ? targetName : node.object.name
+    }
+
+    if (node.type === 'BinaryExpression') {
+      if(node.left) node.left.name = node.left.name === originName ? targetName : node.left.name
+      if(node.right) node.right.name = node.right.name === originName ? targetName : node.right.name
     }
 
     // 继续往下遍历
